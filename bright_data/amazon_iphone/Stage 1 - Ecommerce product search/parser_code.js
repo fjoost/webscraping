4d9ebd62e56e72b0b73fcc61e40cf10943fdaad3
Fernando Joost <desarrollador2@saludaysen.cl>
2021-08-21 18:48:01 -0400

    Initial commit

---
+++ bright_data/amazon_iphone/Stage 1 - Ecommerce product search/parser_code.js
@@ -0,0 +1,3 @@
+return {
+    max_page: +$('.a-pagination .a-disabled').last().text() || +$('.a-pagination .a-normal').last().text()
+};
---
+++ bright_data/v1_scrap_propiedad/interaction_code.js
@@ -0,0 +1,15 @@
+//ir a depto metropolitana
+var i = 0;
+if (i == 0){
+  navigate('https://www.portalinmobiliario.com/MLC-637353847-hermoso-y-amplio-dp-vista-despejada-y-2-dormitorios-grande-_JM#position=1&search_layout=stack&type=item&tracking_id=cb1efb72-c11e-411c-9dc2-afce55188492');
+  //click en las condes
+  //click('#root-app > div > div > aside > section.ui-search-filter-groups > dl:nth-child(4) > dd:nth-child(2) > a > span.ui-search-filter-name');
+  //click en usadas...
+  //click('#root-app > div > div > aside > section.ui-search-filter-groups > dl:nth-child(3) > dd:nth-child(2) > a > span.ui-search-filter-name')
+  //click en primer resultado
+  //click('#root-app > div > div > section > ol > li:nth-child(1) > div > div > div.ui-search-result__content > a > div.ui-search-item__group.ui-search-item__group--price > div > div.ui-search-price__second-line > span > span.price-tag-amount')
+  collect(parse());
+  //collect(parse());
+} else {
+  navigate('https://www.google.com/');
+  }
