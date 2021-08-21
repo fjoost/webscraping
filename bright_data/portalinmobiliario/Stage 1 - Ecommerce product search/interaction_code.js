7979f484ab2c80ec837fed05c990f68d75d4eefa
Fernando Joost <desarrollador2@saludaysen.cl>
2021-08-21 18:50:35 -0400

    copia estructura amazon para portal

---
+++ bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/interaction_code.js
@@ -0,0 +1,11 @@
+if (input.country)
+    country(input.country);
+navigate(`https://www.amazon.com/s?k=${input.search}`);
+let max_page = parse().max_page;
+let final_page = Math.min(input.pages || 0, max_page) || 1;
+for (let i = 1; i <= final_page; i++) {
+    let url = `https://www.amazon.com/s?k=${input.search}`;
+    if (i > 1)
+        url += `&page=${i}`;
+    next_stage({ url, country: input.country, search: input.search });
+}
