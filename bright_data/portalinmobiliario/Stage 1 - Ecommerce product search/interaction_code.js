7ed91f469efb892aac120d987d8d363a288bc002
Fernando Joost <fjoostr@gmail.com>
2021-08-22 23:01:19 -0400

    Recorrido paginacion 90/100

--- bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/interaction_code.js
+++ bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/interaction_code.js
@@ -1,16 +1,27 @@
 /* INPUTS */
 
-// string search = iphone
-// number pages = 10
+// string search = departamento
+// number pages = 40
 
-if (input.country)
-    country(input.country);
-navigate(`https://www.amazon.com/s?k=${input.search}`);
-let max_page = parse().max_page;
-let final_page = Math.min(input.pages || 0, max_page) || 1;
-for (let i = 1; i <= final_page; i++) {
-    let url = `https://www.amazon.com/s?k=${input.search}`;
-    if (i > 1)
-        url += `&page=${i}`;
-    next_stage({ url, country: input.country, search: input.search });
+let url = `https://www.portalinmobiliario.com/venta/departamento`;
+let contador = 0;
+for (let i = 1; i <= input.pages; i++) {
+    if (i == 1) {
+        console.log('vuelta :', i, 'contador :', contador, url);
+        //next_stage({url, country: input.country, search: input.search});
+    }
+    if (i == 2) {
+        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;
+        contador += 51;
+        url += `${contador}`;
+        console.log('vuelta :', i, 'contador :', contador, url);
+        //next_stage({url, country: input.country, search: input.search});
+    }
+    else {
+        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;ﬁ
+        contador += 50;
+        url += `${contador}`;
+        console.log('vuelta :', i, 'contador :', contador, url);
+        //next_stage({url, country: input.country, search: input.search});
+    }
 }
