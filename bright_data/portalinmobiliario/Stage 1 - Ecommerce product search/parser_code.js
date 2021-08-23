f3b0cdbdb42dcc6da1dea42123b61cf2e3521df7
Fernando Joost <fjoostr@gmail.com>
2021-08-22 23:01:19 -0400

    Recorrido paginacion ok

--- bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/parser_code.js
+++ bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/parser_code.js
@@ -1,3 +1,4 @@
-return {
-    max_page: +$('.a-pagination .a-disabled').last().text() || +$('.a-pagination .a-normal').last().text()
-};
+/* return {
+    //max_page: +$('.a-pagination .a-disabled').last().text() || +$('.a-pagination .a-normal').last().text()
+    url
+}; */
