924ae4ad96b47e844fcd8b71bb15785d9eaf85d8
Fernando Joost <fjoostr@gmail.com>
2021-08-24 16:08:37 -0400

    parsea y obtiene por consola número página

--- bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/interaction_code.js
+++ bright_data/portalinmobiliario/Stage 1 - Ecommerce product search/interaction_code.js
@@ -4,9 +4,14 @@
 // number pages = 40
 
 let url = `https://www.portalinmobiliario.com/venta/departamento`;
+navigate(url);
 let contador = 0;
+let page_data = parse();
+//let paginas = parseInt(page_data.Paginass.replace('de ', ''));
 for (let i = 1; i <= input.pages; i++) {
     if (i == 1) {
+//         console.log('paginas :', paginas);
+        console.log(parseInt(page_data.Paginass.replace('de ', '')));
         console.log('vuelta :', i, 'contador :', contador, url);
         //next_stage({url, country: input.country, search: input.search});
     }
@@ -18,7 +23,7 @@ for (let i = 1; i <= input.pages; i++) {
         //next_stage({url, country: input.country, search: input.search});
     }
     else {
-        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;ﬁ
+        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;
         contador += 50;
         url += `${contador}`;
         console.log('vuelta :', i, 'contador :', contador, url);
