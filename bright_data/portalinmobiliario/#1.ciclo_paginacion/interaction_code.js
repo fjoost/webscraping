9d10524e65bc66a6d7f98d5f03acf2f78584a9eb
Fernando Joost <fjoostr@gmail.com>
2021-08-26 19:30:12 -0400

    Departamentos collect listo
    Ahora se debe:
    
    1. Parser code: definir json total de portal o aplicar bifuración dependiendo del tipo de propiedad y tipo negocio (venta o arriendo)
    2. revisar líimite de ciclo = 40 ya que claramente son más

--- bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
+++ bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
@@ -1,32 +1,27 @@
-/* INPUTS */
-
-// string search = departamento
-
-var url1 = `https://www.portalinmobiliario.com/venta/departamento`;
-navigate(url1);
+var url = `https://www.portalinmobiliario.com/venta/departamento`;
+navigate(url);
+let data = parse();
+var paginas = (parseInt(data.Paginass.replace('de ','')));
 let contador = 0;
-let page_data = parse();
-var paginas = (parseInt(page_data.Paginass.replace('de ', '')));
-for (let i = 1; i <= paginas; i++) {
+console.log("paginas: "+paginas);
+for (let i = 1; i<=paginas; i++)
+{
+    var urlPg = url;
     if (i == 1) {
-        console.log('paginas :', paginas);
-//         console.log(parseInt(page_data.Paginass.replace('de ', '')));
-        console.log('vuelta :', i, 'contador :', contador, url);
-        next_stage({url: url1});
+        //navigate(urlPg);
+        collect(parse().items);
     }
-    if (i == 2) {
-        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;
+    else if (i == 2) {
         contador += 51;
-        url += `${contador}`;
-        console.log('vuelta :', i, 'contador :', contador, url);
-        //next_stage({url, country: input.country, search: input.search});
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
     }
     else {
-        let url = `https://www.portalinmobiliario.com/venta/departamento/_Desde_`;
         contador += 50;
-        url += `${contador}`;
-        console.log('vuelta :', i, 'contador :', contador, url);
-        //next_stage({url, country: input.country, search: input.search});
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
     }
 }
 
@@ -43,8 +38,8 @@ for (let i = 1; i<=paginas; i++)
   if (i>1){
     urlPg += `/_Desde_${51*(i-1)}`;
   }
-  console.log(urlPg);
-  //collect(parse().items);
+  navigate(urlPg);
+  collect(parse().items);
 }
 *
 * */
