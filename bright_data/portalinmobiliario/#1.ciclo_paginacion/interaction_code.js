ce8d3c9a629008033e167e752db5b39010e38a78
Fernando Joost <fjoostr@gmail.com>
2021-09-01 10:36:14 -0400

    Inital commit of branch

--- bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
+++ bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
@@ -1,45 +1,34 @@
-var url = `https://www.portalinmobiliario.com/venta/departamento`;
-navigate(url);
-let data = parse();
-var paginas = (parseInt(data.Paginass.replace('de ','')));
-let contador = 0;
-console.log("paginas: "+paginas);
-for (let i = 1; i<=paginas; i++)
-{
-    var urlPg = url;
-    if (i == 1) {
-        //navigate(urlPg);
-        collect(parse().items);
-    }
-    else if (i == 2) {
-        contador += 51;
-        urlPg += `/_Desde_${contador}`;
-        navigate(urlPg);
-        collect(parse().items);
-    }
-    else {
-        contador += 50;
-        urlPg += `/_Desde_${contador}`;
-        navigate(urlPg);
-        collect(parse().items);
-    }
-}
-
-/*
-*
-*var url = `https://www.portalinmobiliario.com/venta`;
-navigate(url);
-let data = parse();
-var paginas = (parseInt(data.Paginass.replace('de ','')));
-console.log("paginas: "+paginas);
-for (let i = 1; i<=paginas; i++)
-{
-  var urlPg = url;
-  if (i>1){
-    urlPg += `/_Desde_${51*(i-1)}`;
-  }
-  navigate(urlPg);
-  collect(parse().items);
-}
-*
-* */
+let regiones = ["antofagasta","arica-y-parinacota","atacama","aysen","biobio",
+    "coquimbo","la-araucania","bernardo-ohiggins","los-lagos","de-los-rios",
+    "magallanes-y-antartica-chilena","maule","metropolitana","tarapaca","valparaiso",
+    "nuble"];
+//console.log(regiones.length);
+let url = `https://www.portalinmobiliario.com/venta`;
+for (let i = 0; i <= regiones.length; i++){
+    url += `/${regiones[i]}`;
+    navigate(url);}
+// let data = parse();
+// let paginas = (parseInt(data.Paginass.replace('de ','')));
+// let tipo_propiedad;
+// let contador = 0;
+// console.log("paginas: "+paginas);
+//   for (let i = 1; i<=paginas; i++)
+// {
+//   let urlPg = url;
+//   if (i == 1) {
+//   	collect(parse().items);
+//     }
+//   else if (i == 2) {
+//     contador += 51;
+//     urlPg += `/_Desde_${contador}`;
+//     navigate(urlPg);
+//     collect(parse().items);
+//   }
+//     else {
+//       contador += 50;
+//       urlPg += `/_Desde_${contador}`;
+//       navigate(urlPg);
+//       collect(parse().items);
+//     }
+// }
+// }
