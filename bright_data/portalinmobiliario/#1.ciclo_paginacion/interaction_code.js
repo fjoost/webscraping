0ba1a59ccce57ae6a5a738729fc61bac9a77bc09
Fernando Joost <fjoostr@gmail.com>
2021-09-01 11:02:47 -0400

    iteración regiones corregida
    ahora trabajar en la iteración conjunta de regiones y paginacion

--- bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
+++ bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
@@ -2,33 +2,33 @@ let regiones = ["antofagasta","arica-y-parinacota","atacama","aysen","biobio",
     "coquimbo","la-araucania","bernardo-ohiggins","los-lagos","de-los-rios",
     "magallanes-y-antartica-chilena","maule","metropolitana","tarapaca","valparaiso",
     "nuble"];
-//console.log(regiones.length);
 let url = `https://www.portalinmobiliario.com/venta`;
-for (let i = 0; i <= regiones.length; i++){
-    url += `/${regiones[i]}`;
-    navigate(url);}
-// let data = parse();
-// let paginas = (parseInt(data.Paginass.replace('de ','')));
-// let tipo_propiedad;
-// let contador = 0;
-// console.log("paginas: "+paginas);
-//   for (let i = 1; i<=paginas; i++)
-// {
-//   let urlPg = url;
-//   if (i == 1) {
-//   	collect(parse().items);
-//     }
-//   else if (i == 2) {
-//     contador += 51;
-//     urlPg += `/_Desde_${contador}`;
-//     navigate(urlPg);
-//     collect(parse().items);
-//   }
-//     else {
-//       contador += 50;
-//       urlPg += `/_Desde_${contador}`;
-//       navigate(urlPg);
-//       collect(parse().items);
-//     }
-// }
-// }
+for (let i = 0; i <= (regiones.length - 1); i++){
+    let urlPg = url;
+    urlPg += `/${regiones[i]}`;
+    console.log(urlPg);}
+let data = parse();
+let paginas = (parseInt(data.Paginass.replace('de ','')));
+let tipo_propiedad;
+let contador = 0;
+console.log("paginas: "+paginas);
+for (let i = 1; i<=paginas; i++)
+{
+    let urlPg = url;
+    if (i == 1) {
+        collect(parse().items);
+    }
+    else if (i == 2) {
+        contador += 51;
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
+    }
+    else {
+        contador += 50;
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
+    }
+}
+}
--- bright_data/portalinmobiliario/#2.scrap_paginacion/parser_code.js
+++ bright_data/portalinmobiliario/#2.scrap_paginacion/parser_code.js
@@ -1,66 +1,34 @@
+let regiones = ["antofagasta","arica-y-parinacota","atacama","aysen","biobio",
+    "coquimbo","la-araucania","bernardo-ohiggins","los-lagos","de-los-rios",
+    "magallanes-y-antartica-chilena","maule","metropolitana","tarapaca","valparaiso",
+    "nuble"];
+let url = `https://www.portalinmobiliario.com/venta`;
+for (let i = 0; i <= (regiones.length - 1); i++){
+    let urlPg = url;
+    urlPg += `/${regiones[i]}`;
+    console.log(urlPg);}
+let data = parse();
+let paginas = (parseInt(data.Paginass.replace('de ','')));
+let tipo_propiedad;
+let contador = 0;
+console.log("paginas: "+paginas);
+for (let i = 1; i<=paginas; i++)
 {
-    "Paginass":{
-    "selector": "li.andes-pagination__page-count",
-        "type": "text"
-},
-    "items": {
-    "multi": true,
-        "selector": "li.ui-search-layout__item",
-        "type": "object",
-        "children": {
-        "tipo_propiedad": {
-            "selector": "span.ui-search-item__group__element.ui-search-item__subtitle",
-                "type": "text"
-        },
-        "ubicacion": {
-            "selector": "p.ui-search-item__group__element.ui-search-item__location",
-                "type": "text"
-        },
-        "es_proyecto": {
-            "selector": "span.ui-search-styled-label.ui-search-item__highlight-label__text",
-                "type": "text"
-        },
-        "moneda": {
-            "selector": "span.price-tag-symbol",
-                "type": "text"
-        },
-        "valor": {
-            "selector": "span.price-tag-fraction",
-                "type": "text"
-        },
-        "m2_utiles": {
-            "selector": "li.ui-search-card-attributes__attribute:nth-child(1)",
-                "type": "text"
-        },
-        "dormitorios": {
-            "selector": "li.ui-search-card-attributes__attribute:nth-child(2)",
-                "type": "text"
-        },
-        "baños": {
-            "selector": "li.ui-search-card-attributes__attribute:nth-child(3)",
-                "type": "text"
-        },
-        "nombre_proyecto": {
-            "selector": "p.ui-search-item__group__element.ui-search-item__information:nth-child(2)",
-                "type": "text"
-        },
-        "tienda_oficial": {
-            "selector": "p.ui-search-official-store-label.official-store--pi.ui-search-color--GRAY",
-                "type": "text"
-        },
-        "nombre_publicacion": {
-            "selector": "p.ui-search-item__group__element.ui-search-item__information:nth-child(3)",
-                "type": "text"
-        },
-        "url": {
-            "selector": "a.ui-search-result__image.ui-search-link",
-                "type": "link"
-        }
-    ,
-        "urlImg": {
-            "selector": "a.ui-search-result__image.ui-search-link",
-                "type": "link"
-        }
+    let urlPg = url;
+    if (i == 1) {
+        collect(parse().items);
+    }
+    else if (i == 2) {
+        contador += 51;
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
+    }
+    else {
+        contador += 50;
+        urlPg += `/_Desde_${contador}`;
+        navigate(urlPg);
+        collect(parse().items);
     }
 }
 }
