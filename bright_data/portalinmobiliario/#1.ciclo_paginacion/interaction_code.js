6c7130950e819b6a3398ec9b7658dc08076b40df
Fernando Joost <fjoostr@gmail.com>
2021-09-01 18:58:45 -0400

    trabajando iteración venta/arriendo
    continuar con validacion paginass<40

--- bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
+++ bright_data/portalinmobiliario/#1.ciclo_paginacion/interaction_code.js
@@ -1,10 +1,13 @@
+//iteracion app arriendo -> i = 219 si paginas < a 40 entonces pagina, si no iteracion aprox
+//iteracion app venta -> i = 1113
+let negocio = ["venta", "arriendo"];
 let regiones = ["antofagasta", "arica-y-parinacota", "atacama", "aysen", "biobio",
     "coquimbo", "la-araucania", "bernardo-ohiggins", "los-lagos", "de-los-rios",
     "magallanes-y-antartica-chilena", "maule", "metropolitana", "tarapaca", "valparaiso",
     "nuble"];
 let url = `https://www.portalinmobiliario.com/venta`;
 for (let i = 0; i <= (regiones.length - 1); i++) {
-    let urlPg = url;
+    var urlPg = url;
     urlPg += `/${regiones[i]}`;
     navigate(urlPg);
     let data = parse();
@@ -12,7 +15,7 @@ for (let i = 0; i <= (regiones.length - 1); i++) {
     let tipo_propiedad;
     let contador = 0;
     console.log("paginas: " + paginas);
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 2; i++) {
         let urlPg2 = urlPg;
         if (i == 1) {
             collect(parse().items);
