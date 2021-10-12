d102e157cd47afced438b620eb8defb0942da746
Fernando Joost <fjoostr@gmail.com>
2021-10-12 10:54:41 -0300

    commit inicial

---
+++ bright_data/toctoc/url_collector/parser_code.js
@@ -0,0 +1,17 @@
+{
+    "Paginass":{
+    "selector": "li.andes-pagination__page-count",
+        "type": "text"
+},
+    "items": {
+    "multi": true,
+        "selector": "li.un-ress tp1",
+        "type": "object",
+        "children": {
+        "url": {
+            "selector": "a.c-imgres",
+                "type": "link"
+        }
+    }
+}
+}
